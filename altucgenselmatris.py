cfb88c8375bb6d4839e1ed51cfbe02bc8d87a3fd
melisabayramli <melisabayramli@hotmail.com>
2017-03-21 20:21:24 +0200

    ustucgenselmatris.py

--- altucgenselmatris.py
+++ altucgenselmatris.py
@@ -9,14 +9,24 @@ for line in dosya.readlines():
     matris.append(line)
 dosya.close()
 boyut = len(matris)
-
 for n in range(boyut):
-    kat = int(matris[n][n])
-    for m in range(boyut+1):
-        matris[n][m]=int(matris[n][m])/kat
-    for p in range(1,boyut-n):
-        kat = float(matris[n+p][n])
-        for q in range(boyut+1):
-            matris[n+p][q]=float(matris[n+p][q])-float(matris[n][q])*(kat/float(matris[n][n]))
-
+        kat = int(matris[n][n])
+        for m in range(boyut+1):
+           if kat!=0:
+                matris[n][m]=int(matris[n][m])/kat
+        for p in range(1,boyut-n):
+            kat = float(matris[n+p][n])
+            for q in range(boyut+1):
+                if matris[n][n]!=0:
+                      matris[n+p][q]=float(matris[n+p][q])-float(matris[n][q])*(kat/float(matris[n][n]))
+        for t in range(1,boyut-n):
+            kat=float(matris[n+t][t])
+            for b in range(1,boyut+1):
+                if matris[n][n]!=0:
+                    matris[n][b]=float(matris[n][b])-float(matris[n][b])*(kat/float(matris[n][n]))
+            for k in range(1,boyut-1):
+                if matris[n][n]!=0:
+                    matris[k][2]=float(matris[k][2])-float(matris[k][2])*(kat/float(matris[n][n]))
+        
 print(matris)
+
