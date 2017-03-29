e7876b7ef57ee26e13c1e00aec52c7711e06bdb2
melisabayramli <melisabayramli@hotmail.com>
2017-03-29 20:20:17 +0300

    polinom

--- polinom-butunkokler.py
+++ polinom-butunkokler.py
@@ -1,9 +1,38 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+from math import *
+katsayilar=[1.0,-7.0,-3.0,79.0,-46.0,-120.0]
+print(katsayilar)
+yeni=[]
 def f(x):
-    return(1*x**5-7*x**4-3*x**3+79*x**2-46*x-120)
+    fonk=0
+    for i in range(len(katsayilar)):
+        fonk=fonk+katsayilar[i]*pow(x,len(katsayilar)-i)
+    return(fonk)
+def fi(x):
+    fonk1=0
+    for i in range(len(katsayilar)-1):
+        fonk1=fonk1+katsayilar[i]*(len(katsayilar)-i-1)*pow(x,len(katsayilar)-i-1)
+    return(fonk1)
 
-xi = float(input("bir başlangıç değeri girin: "))
+def kok(x):
+    for i in range(100):
+      if fi(x)!=0:
+          x=x-f(x)/fi(x)
+    return(x)
+xi=float(input("bir baslangic degeri girin:"))
+while(len(katsayilar)!=2):
+            k=kok(xi)
+            xi=k
+            yeni.append(k)
+            for j in range(len(katsayilar)):
+                if(j==0):
+                        continue
+                katsayilar[j]=k*(katsayilar[j-1])+katsayilar[j]
+            katsayilar=katsayilar[:len(katsayilar)-1]
+            if(len(katsayilar)==2):
+                t=-katsayilar[1]/katsayilar[0]
+                yeni.append(t)
+            print(yeni)            
 
+for i in range(5):
+    print("%d. kok="%(i+1),yeni[i])
 
-print(x1,x2,x3,x4,x5)
